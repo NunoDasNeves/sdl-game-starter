d5a42801a170e51acace54cb1a65fee82d893504
Nuno Das Neves <ndasneves@gmail.com>
2020-01-21 09:21:39 -0800

    update linux build script

--- build.sh
+++ build.sh
@@ -7,10 +7,14 @@ NAME=sdl-game-starter
 SOURCES=$(ls ../src/*.cpp)
 OBJS=$(find ../src/ -name *.cpp -printf '%f\n' | sed s/cpp/o/g)
 
+OTHER_FLAGS="-DSTDOUT_DEBUG -DFIXED_GAME_MEMORY"
+COMPILER_FLAGS="-c -Wall"
+LINKER_FLAGS=""
+
 echo compiling
 for src in ${SOURCES}; do
     echo "  $src"
-    g++ ${src} -c -Wall || exit 1
+    g++ ${src} ${COMPILER_FLAGS} ${OTHER_FLAGS} || exit 1
 done
 echo linking
 g++ ${OBJS} -lSDL2 -o ${NAME} || exit 1
